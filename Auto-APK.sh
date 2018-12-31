47355cd6af1de740b895451b1fd1399e4b49d782
AgoniNemo <XZC168520@163.com>
2018-12-31 20:59:47 +0800

    更新打包脚本

--- Auto-APK.sh
+++ Auto-APK.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+#  android编译为原生命令
 react-native bundle --platform android --dev false --entry-file index.js  --bundle-output android/app/src/main/assets/index.android.bundle  --assets-dest android/app/src/main/res/
 
+#  打包APK命令
 cd android && ./gradlew assembleRelease
 
 cd ..
